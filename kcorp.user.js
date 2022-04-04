4025822d047b0b21487437eb13521cdce1ee17a7
cris99oy <102991379+cris99oy@users.noreply.github.com>
2022-04-04 15:01:32 -0500

    Update kcorp.user.js

--- kcorp.user.js
+++ kcorp.user.js
@@ -1,19 +1,16 @@
 // ==UserScript==
-// @name         Reddit Place - Armée de Kameto
-// @namespace    http://tampermonkey.net/
+// @name         Reddit Place 
+// @namespace    
 // @version      0.1
-// @description  On va récuperer ce qui nous est du de droit.
-// @author       Adcoss95
-// @match        https://hot-potato.reddit.com/embed*
-// @icon         https://styles.redditmedia.com/t5_4eiiz1/styles/communityIcon_ojy24r8j90o81.jpg
-// @grant        none
+// @description  recupera la bandera
+// @author       CRIS99
 // ==/UserScript==
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://github.com/CorentinGC/reddit-place-kcorp/raw/main/overlay.png";
+            i.src = "https://github.com/cris99oy/reddit-place/raw/main/redditperu.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
