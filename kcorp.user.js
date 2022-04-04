f830e703e75f3781cc73ebd0b4940b34b948a383
cris99oy <102991379+cris99oy@users.noreply.github.com>
2022-04-04 15:54:08 -0500

    Update kcorp.user.js

--- kcorp.user.js
+++ kcorp.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://github.com/cris99oy/reddit-place/raw/main/redditperu.png";
+            i.src = "https://github.com/cris99oy/reddit-place/raw/main/redditperu9.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             document.addEventListener("keydown", function(event) {
